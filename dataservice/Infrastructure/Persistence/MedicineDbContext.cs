6b4b1f99ca8a215dd2a2fe331aea86a1be7a426d
Niels van Hal <ntj.vanhal@student.avans.nl>
2022-03-28 11:39:19 +0200

    model+db setup

---
+++ dataservice/Infrastructure/Persistence/MedicineDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities.MedicalData;
+using Infrastructure.Persistence.Seeders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    internal class MedicineDbContext : DbContext
+    {
+        public DbSet<Medicine> Medicine { get; set; }
+        public DbSet<MedicineColor> Medicine_Colors { get; set; }
+        public DbSet<MedicineShape> Medicine_Shapes { get; set; }
+        public DbSet<MedicineType> Medicine_Types { get; set; }
+        public DbSet<DoseUnit> DoseUnits { get; set; }
+
+        public MedicineDbContext(DbContextOptions<MedicineDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Medicine>()
+                .HasOne<MedicineColor>()
+                .WithMany()
+                .HasForeignKey(m => m.Color);
+
+            modelBuilder.Entity<Medicine>()
+                .HasOne<MedicineShape>()
+                .WithMany()
+                .HasForeignKey(m => m.Shape);
+
+            modelBuilder.Entity<Medicine>()
+                .HasOne<MedicineType>()
+                .WithMany()
+                .HasForeignKey(m => m.Type);
+
+            modelBuilder.Entity<Medicine>()
+                .HasOne<DoseUnit>()
+                .WithMany()
+                .HasForeignKey(m => m.DoseUnit);
+
+            MedicineIdentifierSeeder.SeedMedicineIdentifiers(modelBuilder);
+            MedicineSeeder.SeedMedicine(modelBuilder);
+        }
+    }
+}
---
+++ dataservice/Infrastructure/Persistence/PatientDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    internal class PatientDbContext : DbContext
+    {
+        public PatientDbContext(DbContextOptions<PatientDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
