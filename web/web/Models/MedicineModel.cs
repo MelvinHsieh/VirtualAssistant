f9e1ec60c8711e9056de9aea085646a5974fc528
Wesley Salimans <wlt.salimans@student.avans.nl>
2022-04-04 08:21:03 +0200

    list medicine

---
+++ web/web/Models/MedicineModel.cs
@@ -0,0 +1,14 @@
+﻿namespace web.Models
+{
+    public class MedicineModel
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public string Indication { get; set; } = string.Empty;
+        public int Dose { get; set; } = 0;
+        public string DoseUnit { get; set; } = string.Empty;
+        public string Shape { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+    }
+}
