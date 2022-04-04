6d6beea9e3307e2b627bfdac1625fbccd9aad8b3
Wesley Salimans <wlt.salimans@student.avans.nl>
2022-04-04 15:07:13 +0200

    thank you niels

--- web/web/Controllers/MedicineController.cs
+++ web/web/Controllers/MedicineController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using web.Models;
 
 namespace web.Controllers
 {
     public class MedicineController : Controller
     {
-        // GET: MedicineController
-        public ActionResult Index()
+        private readonly String _apiURL;
+
+        public MedicineController(IConfiguration configuration)
         {
-            return View();
+            _apiURL = configuration.GetValue<String>("DataServiceURL");
+            Console.WriteLine(_apiURL);
+        }
+
+        // GET: MedicineController
+        public async Task<ActionResult> IndexAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var uri = new Uri(_apiURL + "/Medicine");
+
+                var response = client.GetAsync(uri).Result;
+               
+                string result = await response.Content.ReadAsStringAsync();
+
+                List<MedicineModel>? models = JsonConvert.DeserializeObject<List<MedicineModel>>(result);
+                return View(models);
+            }
         }
 
         // GET: MedicineController/Details/5
@@ -18,19 +38,51 @@ namespace web.Controllers
         }
 
         // GET: MedicineController/Create
-        public ActionResult Create()
+        public async Task<ActionResult> CreateAsync()
         {
-            return View();
+            using(var client = new HttpClient())
+            {
+                var uri = new Uri(_apiURL + "/DoseUnit");
+                var response = client.GetAsync(uri).Result;
+                string result = await response.Content.ReadAsStringAsync();
+
+                List<String>? doseUnits = JsonConvert.DeserializeObject<List<String>>(result);
+
+                uri = new Uri(_apiURL + "/MedicineColor");
+                response = client.GetAsync(uri).Result;
+                result = await response.Content.ReadAsStringAsync();
+
+                List<String>? medicineColors = JsonConvert.DeserializeObject<List<String>>(result);
+
+                uri = new Uri(_apiURL + "/MedicineType");
+                response = client.GetAsync(uri).Result;
+                result = await response.Content.ReadAsStringAsync();
+
+                List<String>? medicineTypes = JsonConvert.DeserializeObject<List<String>>(result);
+
+                uri = new Uri(_apiURL + "/MedicineShape");
+                response = client.GetAsync(uri).Result;
+                result = await response.Content.ReadAsStringAsync();
+
+                List<String>? medicineShapes = JsonConvert.DeserializeObject<List<String>>(result);
+
+                ViewBag.DoseUnits = doseUnits;
+                ViewBag.MedicineColors = medicineColors;
+                ViewBag.MedicineTypes = medicineTypes;
+                ViewBag.MedicineShapes = medicineShapes;
+
+                return View();
+            }
         }
 
         // POST: MedicineController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(MedicineModel model)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(IndexAsync));
             }
             catch
             {
@@ -51,7 +103,7 @@ namespace web.Controllers
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(IndexAsync));
             }
             catch
             {
@@ -72,7 +124,7 @@ namespace web.Controllers
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(IndexAsync));
             }
             catch
             {
--- web/web/Models/MedicineModel.cs
+++ web/web/Models/MedicineModel.cs
@@ -1,14 +1,24 @@
-﻿namespace web.Models
+﻿using Newtonsoft.Json;
+
+namespace web.Models
 {
     public class MedicineModel
     {
+        [JsonProperty(PropertyName = "id")]
         public int Id { get; set; } = 0;
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; } = string.Empty;
+        [JsonProperty(PropertyName = "indication")]
         public string Indication { get; set; } = string.Empty;
-        public int Dose { get; set; } = 0;
+        [JsonProperty(PropertyName = "dose")]
+        public double Dose { get; set; } = 0;
+        [JsonProperty(PropertyName = "doseUnit")]
         public string DoseUnit { get; set; } = string.Empty;
+        [JsonProperty(PropertyName = "shape")]
         public string Shape { get; set; } = string.Empty;
+        [JsonProperty(PropertyName = "color")]
         public string Color { get; set; } = string.Empty;
+        [JsonProperty(PropertyName = "type")]
         public string Type { get; set; } = string.Empty;
     }
 }
