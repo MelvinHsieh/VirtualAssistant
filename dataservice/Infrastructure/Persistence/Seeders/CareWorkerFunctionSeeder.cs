c9aeab27d1b6d7503d305891bd91d64d3a0cf0d1
Wesley Salimans <wlt.salimans@student.avans.nl>
2022-05-03 19:35:50 +0200

    model and seeder creation

---
+++ dataservice/Infrastructure/Persistence/Seeders/CareWorkerFunctionSeeder.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.MedicalData;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Seeders
+{
+    internal static class CareWorkerFunctionSeeder
+    {
+        public static void SeedCareWorkerFunction(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CareWorkerFunction>().HasData(
+                new CareWorkerFunction() { Name = "Verpleger" },
+                new CareWorkerFunction() { Name = "Dokter" }
+                );
+        }
+    }
+}
