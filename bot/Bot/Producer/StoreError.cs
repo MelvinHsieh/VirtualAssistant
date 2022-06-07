92ecc71cfd80cef259094b6b6f7dfefb1a753e5d
Wesley Salimans <wlt.salimans@student.avans.nl>
2022-06-07 11:30:13 +0200

    enable logging

--- bot/Bot/Producer/StoreError.cs
+++ bot/Bot/Producer/StoreError.cs
@@ -28,30 +28,30 @@ namespace CoreBot.Producer
 
         public void SendMessage<T>(T message)
         {
-            //var factory = new ConnectionFactory
-            //{
-            //    UserName = USERNAME,
-            //    Password = PASSWORD,
-            //    HostName = _configuration.GetSection("MessageBus").Value,
-            //    Port = Int32.Parse(_configuration.GetSection("MessageBusPort").Value)
-            //};
+            var factory = new ConnectionFactory
+            {
+                UserName = USERNAME,
+                Password = PASSWORD,
+                HostName = _configuration.GetSection("MessageBus").Value,
+                Port = Int32.Parse(_configuration.GetSection("MessageBusPort").Value)
+            };
 
-            //var connection = factory.CreateConnection();
-            //using var channel = connection.CreateModel();
+            var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
 
-            //channel.ExchangeDeclare(exchange: EXCHANGE_NAME, type: ExchangeType.Direct, true);
+            channel.ExchangeDeclare(exchange: EXCHANGE_NAME, type: ExchangeType.Direct, true);
 
-            //channel.QueueDeclare(QUEUE_NAME, true, false, false, null);
-            //channel.QueueBind(QUEUE_NAME, EXCHANGE_NAME, QUEUE_NAME);
+            channel.QueueDeclare(QUEUE_NAME, true, false, false, null);
+            channel.QueueBind(QUEUE_NAME, EXCHANGE_NAME, QUEUE_NAME);
 
 
-            //var json = JsonConvert.SerializeObject(message);
-            //var body = Encoding.UTF8.GetBytes(json);
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
 
-            //channel.BasicPublish(exchange: EXCHANGE_NAME, routingKey: QUEUE_NAME, body: body);
-
-            //channel.Close();
-            //connection.Close();
+            channel.BasicPublish(exchange: EXCHANGE_NAME, routingKey: QUEUE_NAME, body: body);
+            
+            channel.Close();
+            connection.Close();
         }
     }
 }
