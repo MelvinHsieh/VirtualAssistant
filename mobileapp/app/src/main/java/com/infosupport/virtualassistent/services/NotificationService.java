c945eb14c981010b3e1c1fccaf8b30c5f40a44e7
Merijn Monfils <merijn.monfils@gmail.com>
2022-06-09 19:26:32 +0200

    EoD
    leaving changes for tommorow

--- mobileapp/app/src/main/java/com/infosupport/virtualassistent/services/NotificationService.java
+++ mobileapp/app/src/main/java/com/infosupport/virtualassistent/services/NotificationService.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.preference.PreferenceManager;
@@ -17,9 +18,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.firebase.installations.FirebaseInstallations;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.infosupport.virtualassistent.MainActivity;
 import com.infosupport.virtualassistent.R;
 
 import java.util.HashMap;
@@ -27,63 +30,40 @@ import java.util.Map;
 import java.util.Objects;
 
 public class NotificationService extends FirebaseMessagingService {
-    /*private final String URL_DEVICE;
-    private final RequestQueue queue;
-    private final SharedPreferences preferences;*/
-    private final String CHANNEL_ID = "HANS Notification Channel";
+    private final String URL_DEVICE;
 
-    /*NotificationService(Activity activity) {
-        activity = (activity == null) ? (Activity) this.getApplicationContext() : activity;
-        URL_DEVICE = activity.getApplicationContext().getString(R.string.dataservice_url) + "/api/PatientDevice";
-        queue = Volley.newRequestQueue(activity);
-        preferences = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
-    }*/
+    NotificationService() {
+        URL_DEVICE = R.string.dataservice_url + "/api/PatientDevice";
+        FirebaseInstallations.getInstance().getId().addOnCompleteListener(
+                task -> {
+                    if (task.isSuccessful()) {
+                        String token = task.getResult();
+                        Log.i("token ---->>", token);
+
+                        // store the token in shared preferences
+                        PrefUtils.getInstance(getApplicationContext()).setValue(PrefKeys.FCM_TOKEN, token);
+                    }
+                }
+        );
+    }
 
     @Override
     public void onNewToken(@NonNull String token) {
         // Log new token
         /*sendRegistrationToServer(token);*/
-        Log.d("Token", "Refreshed token: " + token);
+        System.out.print("Refreshed token: " + token);
         super.onNewToken(token);
     }
 
-    @Override
-    public void onMessageReceived(@NonNull RemoteMessage message) {
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d("MEssage", "From: " + message.getFrom());
-
-        // Check if message contains a data payload.
-        if (message.getData().size() > 0) {
-            Log.d("Payload", "Message data payload: " + message.getData());
-
-            /*if (*//* Check if data needs to be processed by long running job *//* true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                handleNow();
-            }*/
-
-        }
-
-        // Check if message contains a notification payload.
-        if (message.getNotification() != null) {
-            Log.d("Body", "Message Notification Body: " + message.getNotification().getBody());
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-        /*String title = Objects.requireNonNull(message.getNotification()).getTitle();
-        String text = message.getNotification().getBody();
-        createNotificationChannel(title, text);*/
+    // either use below function to get the token or directly get from the shared preferences
+    public static String getToken(Context context) {
+        return PrefUtils.getInstance(context).getStringValue(PrefKeys.FCM_TOKEN, "");
     }
 
     /** Creates notification channel **/
     private void createNotificationChannel(String title, String text) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String CHANNEL_ID = "HANS Notification Channel";
             NotificationChannel serviceChannel = new NotificationChannel(
                     CHANNEL_ID,
                     "HANS Notification",
